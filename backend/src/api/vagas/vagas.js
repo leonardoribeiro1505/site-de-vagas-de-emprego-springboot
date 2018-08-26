0cb957cf390a9a39e6b69e84bee19027355ff6f0
Leonardo Ribeiro <leo.ribeiro.as@gmail.com>
2018-08-26 10:56:55 -0300

    Pequenas correções

--- backend/src/api/vagas/vagas.js
+++ backend/src/api/vagas/vagas.js
@@ -3,11 +3,11 @@ const mongoose = restful.mongoose
 
 const vagasSchema = new mongoose.Schema({
     nomeCargo: {  type: String, required: true },
-    tipoContratacao: {  type: String, required: true },
+    tipoContratacao: {  type: String, required: false },
     cargaHoraria: {  type: String, required: false },
     salario: {  type: String, required: false },
-    tempoExibicao: {  type: Date, required: true },
-    descricao: {  type: String, required: true },
+    tempoExibicao: {  type: Date, required: false },
+    descricao: {  type: String, required: false }
 })
 
 module.exports = restful.model('Vagas', vagasSchema)
