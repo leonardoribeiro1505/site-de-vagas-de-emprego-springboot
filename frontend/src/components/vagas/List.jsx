4f4e82b614c69ff490c5beca740e8a1d5daab8e9
Leonardo Ribeiro <leo.ribeiro.as@gmail.com>
2018-09-19 16:25:04 -0300

    Add o react router e o set da vaga pelo id

--- frontend/src/components/vagas/List.jsx
+++ frontend/src/components/vagas/List.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { List, Card } from 'antd';
 import axios from 'axios';
 import imageAvatar from '../../assets/images/mslogo.jpg'
+import { Link } from 'react-router-dom'
 
 const { Meta } = Card;
 
@@ -19,30 +20,29 @@ export default class VagasList extends React.Component {
       }
 
     render() {
-        console.log(this.state.list);
-        const listData = this.state.list;
+        const listData = (this.state.list || []).map(i => ({...i, key: i.id}));
         return (
-            <div style={{ marginLeft: 100, marginRight: 100 }}>
-            <br/>
-            <h1>Vagas disponíveis</h1>
-            <br/>
-                <List
-                    itemLayout="horizontal"
-                    dataSource={listData}
-                    renderItem={item => (
-                    <Card
-                        hoverable
-                        style={{ width: 240 }}
-                        cover={<img alt="example" src={imageAvatar} />}
-                    >
-                        <Meta
-                        title={item.nomeCargo}
-                        description={`Contratação: ${item.tipoContratacao}`}
-                        />
-                    </Card>
-                )}
-                />
-            </div>
+            <center>
+                    <br/>
+                    <h1>Vagas disponíveis</h1>
+                    <br/>
+                        <List
+                            itemLayout="horizontal"
+                            dataSource={listData}
+                            renderItem={item => (
+                            <Card
+                                hoverable
+                                style={{ width: 240 }}
+                                cover={<Link to={`/details/${item._id}`}><img width="238" alt="example" src={imageAvatar} /></Link>}
+                            >
+                                <Meta
+                                title={<Link to="/details">{item.nomeCargo}</Link>}
+                                description={<Link to="/details">Contratação: {item.tipoContratacao}</Link>}
+                                />
+                            </Card>
+                        )}
+                        />                
+            </center>
         );
     }
 }
