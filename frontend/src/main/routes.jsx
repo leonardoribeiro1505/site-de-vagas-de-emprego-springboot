e920360790ba976466ed3c4ef2b39349348f4c42
Leonardo Ribeiro <leonardoribeiro@Mac-mini-de-Insight.local>
2018-09-20 11:15:22 -0300

    Melhorias nas rotas

--- frontend/src/main/routes.jsx
+++ frontend/src/main/routes.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Route, Switch, HashRouter } from 'react-router-dom'
+import { Route, Switch } from 'react-router-dom'
 
 import VagasList from '../components/vagas/List'
 import CadVagasForm from '../components/vagas/Form'
@@ -8,7 +8,6 @@ import LoginForm from '../components/login/Form'
 import DetailsVaga from '../components/vagas/Details'
 
 export default props =>
-    <HashRouter>
         <Switch>
             <Route exact path="/" component={VagasList} />
             <Route path="/cadVagasForm" component={CadVagasForm} />
@@ -16,4 +15,4 @@ export default props =>
             <Route path="/about" component={About} />
             <Route path="/details" component={DetailsVaga} />
         </Switch>
-    </HashRouter>
+    
