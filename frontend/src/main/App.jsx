24fdddee11494a3e7d3176799d2babffbeec1bd5
Leonardo Ribeiro <leo.ribeiro.as@gmail.com>
2018-09-21 17:24:44 -0300

    Add o componente de rodapé

--- frontend/src/main/App.jsx
+++ frontend/src/main/App.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
-
 import { BrowserRouter } from 'react-router-dom'
 import MenuNav from '../template/MenuNav'
 import Routes from './routes'
+import PageFooter from '../template/pageFooter'
 
 export default class App extends React.Component {
     render() {
@@ -11,6 +11,11 @@ export default class App extends React.Component {
                 <div className="container">
                     <MenuNav />
                     <Routes />
+                    <PageFooter  
+                        paragraph1='&copy; Copyrights '
+                        featured=' EquipeVagasDeEmprego. '
+                        paragraphKeep='Todos os direitos reservados'
+                        paragraph2='Desenvolvido na disciplina de Tecnologias para Internet II' />
                 </div>
             </BrowserRouter>
     )
