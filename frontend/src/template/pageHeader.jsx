1e9831be9a08717e9e47f2e5b12a63225716ecd7
Leonardo Ribeiro <leo.ribeiro.as@gmail.com>
2018-09-21 17:25:32 -0300

    Criação do componente de rodapé

--- frontend/src/template/pageHeader.jsx
+++ frontend/src/template/pageHeader.jsx
@@ -3,6 +3,8 @@ import React from 'react'
 export default props =>
     <header style={{ background: '#3498db', padding: '37px', color: '#FFFFFF' }}>
         <center>
-            <h1 style={{ color: '#FFFFFF' }}>{props.name}</h1>
+            <h1 style={{ color: '#FFFFFF' }}>
+                {props.name}
+            </h1>
         </center>
     </header>
