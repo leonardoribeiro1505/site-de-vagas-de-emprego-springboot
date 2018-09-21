e37cc39995ea11e8bb9b27c1585f34df4a916512
Leonardo Ribeiro <leo.ribeiro.as@gmail.com>
2018-09-21 17:27:40 -0300

    Criação do componente de rodapé

---
+++ frontend/src/template/pageFooter.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export default props =>
+    <footer style={{ background: '#363636', 
+        padding: '37px', 
+        position: 'absolute', 
+        bottom: '0', 
+        width: '100%', 
+        color: '#FFFFFF' }}>
+        <center>
+            <p style={{ color: '#FFFFFF' }}>
+                {props.paragraph1}
+                <strong>{props.featured}</strong>
+                {props.paragraphKeep}
+            </p>
+            <p style={{ color: '#FFFFFF' }}>
+                {props.paragraph2}
+            </p>
+        </center>
+    </footer>
