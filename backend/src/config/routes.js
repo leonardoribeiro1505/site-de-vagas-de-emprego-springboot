4409e6b7badff842123ac37decdcd8beac1577fa
Leonardo Ribeiro <leo.ribeiro.as@gmail.com>
2018-09-23 09:09:33 -0300

    Finalização da autenticação de usuários

--- backend/src/config/routes.js
+++ backend/src/config/routes.js
@@ -1,16 +1,32 @@
 const express = require('express')
+const auth = require('./auth')
 
 module.exports = function(server) {
 
-    //API Routes
-    const router = express.Router()
-    server.use('/api', router)
+    /*
+    * Rotas protegidas por Token JWT
+    */
+    const protectedApi = express.Router()
+    server.use('/api', protectedApi)
 
+    protectedApi.use(auth)
+    
     //VAGAS Routes
     const vagasService = require('../api/vagas/vagasService')
-    vagasService.register(router, '/vagas')
+    vagasService.register(protectedApi, '/vagas')
 
-    //USUARIOS Routes
-    const usuariosService = require('../api/login/usuariosService')
-    usuariosService.register(router, '/usuarios')
+    /*
+    * Rotas abertas
+    */
+    const openApi = express.Router()
+    server.use('/oapi', openApi)
+
+    const AuthService = require('../api/user/authService')
+    openApi.post('/login', AuthService.login)
+    openApi.post('/signup', AuthService.signup)
+    openApi.post('/validateToken', AuthService.validateToken)
+
+    // //USUARIOS Routes
+    // const usuariosService = require('../api/login/usuariosService')
+    // usuariosService.register(router, '/usuarios')
 }
