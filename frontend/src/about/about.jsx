ad1e3c7911ac318221853a85477576e10feb39bd
Leonardo Ribeiro <leonardoribeiro@Mac-mini-de-Insight.local>
2018-10-08 09:40:39 -0300

    Melhorias diversas no layout

--- frontend/src/about/about.jsx
+++ frontend/src/about/about.jsx
@@ -1,73 +1,38 @@
 import React from 'react'
 import { Row, Col, Card, Avatar, Icon } from 'antd';
-import imageAvatar from '../assets/images/avatar.png'
+import imageAvatar from '../assets/images/FotoPerfil.png'
 
 const { Meta } = Card;
 
 export default props =>
 
     <div>
-        <div style={{ background: '#3498db', padding: '30px', color: '#FFFFFF' }}>
-        <center>
-            <p>SAIBA UM POUCO MAIS SOBRE NÓS</p>
+        <div style={{ background: '#3498db', padding: '30px', color: '#FFFFFF', textAlign: 'center' }}>
             <p>SOMOS UM SITE PARA ANÚNCIO DE VAGAS DE EMPREGO</p>
-        </center>
+            <p>DESENVOLVIDO NA DISICPLINA DE TECNOLOGIAS PARA INTERNET II</p>
+            <p>AINDA EM DESENVOLVIMENTO...</p>
         </div>
         <br/>
         <div style={{ background: '#ECECEC', padding: '30px' }}>
-
-            <Row gutter={16}>
-                <Col span={6}>
-                    <Card
-                        style={{ width: 300, marginTop: 16 }}
-                        actions={[
-                            <a href="http://www.linkedin.com/in/leonardo-ribeiro-as/" target="blank"><Icon type="linkedin" /></a>, 
-                        <a href="http://github.com/leonardoribeiro1986" target="blank"><Icon type="github" /></a>, 
-                        <Icon type="mail" />]}
-                    >
-                        <Meta
-                            avatar={<Avatar src={imageAvatar} size={64}/>}
-                            title="Leonardo Ribeiro"
-                            description="This is the description"
-                        />
-                    </Card>
-                </Col>
-                <Col span={6}>
-                <Card
-                        style={{ width: 300, marginTop: 16 }}
-                        actions={[<Icon type="linkedin" />, <Icon type="github" />, <Icon type="mail" />]}
-                    >
-                        <Meta
-                            avatar={<Avatar src={imageAvatar} size={64} />}
-                            title="Renato Braúna"
-                            description="This is the description"
-                        />
-                    </Card>
-                </Col>
-                <Col span={6}>
-                <Card
-                        style={{ width: 300, marginTop: 16 }}
-                        actions={[<Icon type="linkedin" />, <Icon type="github" />, <Icon type="mail" />]}
-                    >
-                        <Meta
-                            avatar={<Avatar src={imageAvatar} size={64} />}
-                            title="Júnior"
-                            description="This is the description"
-                        />
-                    </Card>
-                </Col>
-                <Col span={6}>
-                <Card
-                        style={{ width: 300, marginTop: 16 }}
-                        actions={[<Icon type="linkedin" />, <Icon type="github" />, <Icon type="mail" />]}
-                    >
-                        <Meta
-                            avatar={<Avatar src={imageAvatar} size={64} />}
-                            title="Card title"
-                            description="This is the description"
-                        />
-                    </Card>
-                </Col>
-            </Row>
+            <center>
+                <Row gutter={16}>
+                    <Col>
+                        <Card
+                            style={{ width: 300, marginTop: 16 }}
+                            actions={[
+                                <a href="http://www.linkedin.com/in/leonardo-ribeiro-as/" target="blank"><Icon type="linkedin" /></a>, 
+                                <a href="http://github.com/leonardoribeiro1986" target="blank"><Icon type="github" /></a>, 
+                                <a href="mailto:leo.ribeiro.as@gmail.com" ><Icon type="mail" /></a>]}
+                        >
+                            <Meta
+                                avatar={<Avatar src={imageAvatar} size={64}/>}
+                                title="Leonardo Ribeiro"
+                                description="Dsenvolvedor Full Stack"
+                            />
+                        </Card>
+                    </Col>
+                    
+                </Row>
+            </center>
         </div>
     </div>
