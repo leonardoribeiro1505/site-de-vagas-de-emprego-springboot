12295b5aecb3509debd9f2fc493359d41bb4e2c2
Leonardo Ribeiro <leo.ribeiro.as@gmail.com>
2018-10-08 17:20:55 -0300

    padronização do calendário para pt-BR

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,8 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import 'antd/dist/antd.css';
+import { LocaleProvider } from 'antd';
+import pt_BR from 'antd/lib/locale-provider/pt_BR'
+import 'moment/locale/pt-br'
 import App from './main/App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+<LocaleProvider locale={pt_BR}>
+    <App />
+</LocaleProvider>, document.getElementById('root'));
 registerServiceWorker();
